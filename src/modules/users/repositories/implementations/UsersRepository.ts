731bff4e10825e8583647da2236c76c438a8c221
Paulo Henrique <phferreirasouza106@gmail.com>
2021-10-22 08:28:57 -0300

    challenge has been completed

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,23 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    return this.repository.findOne({
+      where: { id: user_id },
+      relations: ["games"]
+    });
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users ORDER BY first_name, last_name ASC"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users where lower(users.first_name) = $1 AND lower(users.last_name) = $2", [
+      first_name.toLowerCase(),
+      last_name.toLowerCase(),
+    ]);
   }
 }
